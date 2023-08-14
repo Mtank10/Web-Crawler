afa9602771231c6ce45eff73e0032989c15e6710
Mtank10 <rajshivam691@gmail.com>
2023-08-14 14:00:44 +0530

    add more test for normalizURL

---
+++ crawl.test.js
@@ -0,0 +1,29 @@
+const {normalizeURL} = require('./crawl.js')
+const {test,expect} = require('@jest/globals');
+
+test('normalizURL strip protocol',()=>{
+    const input = 'https://blog.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected)
+})
+
+test('normalizURL strip trailing slash',()=>{
+    const input = 'https://blog.boot.dev/path/';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected)
+})
+test('normalizURL capitals',()=>{
+    const input = 'https://BLOG.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected)
+})
+
+test('normalizURL strip http',()=>{
+    const input = 'https://blog.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected)
+})
