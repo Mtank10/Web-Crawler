8e152ecf68ae3dc5483d92c86d1b20c57ca4ca03
Mtank10 <rajshivam691@gmail.com>
2023-08-17 17:51:26 +0530

    add thread worker to fast web crawling

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const {crawlPage} =require('./crawl.js');
 const {printReport} = require('./report.js')
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 async function main(){
-  if(process.argv.length<3){
-    console.log("no website provided");
-    process.exit(1);
-  }
+//   if(process.argv.length<3){
+//     console.log("no website provided");
+//     process.exit(1);
+//   }
 //   if(process.argv.length>3){
 //     console.log("too many command line args");
 //     process.exit(1);
@@ -12,11 +13,17 @@ async function main(){
 //   for(const arg of process.argv){
 //     console.log(arg);
 //   }
-  for(let i=2;i<process.argv.length;i++){
-      var baseURL = process.argv[i];
-  
-  console.log(`starting crawl of ${baseURL}`);
-  const pages =await crawlPage(baseURL,baseURL,{});
+   
+   if(isMainThread){
+    const urls = process.argv.slice(2);
+   console.log(urls);
+    urls.forEach(url=>{
+        new Worker(__filename,{workerData:url})
+    });
+   }else{
+    const url=workerData;  
+  console.log(`starting crawl of ${url}`);
+  const pages =await crawlPage(url,url,{});
   for(const page of Object.entries(pages)){
     printReport(pages)
   }
